9c3113e24e140b04215f5944181abafe228f3c65
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-16 05:28:09 +0000

    add loadTweets function in client.js to fetch (GET) data from server using AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function () {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -40,6 +15,18 @@ $(document).ready(function () {
       $("#tweets-container").append($eachTweet);
     }
   };
+
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:8080/tweets",
+      complete: function(data){
+        renderTweets(data.responseJSON);
+      }
+    });
+  };
+
+  loadTweets();
   
   const createTweetElement = function(tweet) {
     const date = timeago.format(tweet.created_at);
@@ -71,12 +58,13 @@ $(document).ready(function () {
   $("#tweetCompose").submit(function (event) {
     event.preventDefault();
     const data = $(this).serialize();
-    console.log('test');
     $.ajax({
       method: "POST",
       url: "http://localhost:8080/tweets/",
-      data
+      data,
+      success: function() {
+        console.log('Data was succesfully captured');
+      }
     });
   });
-  renderTweets(data);
 });
